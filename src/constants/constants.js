3503fa81cd5c8dcc9926d4e30aaaa800789faff1
Daniel Zazzali <dazazzali@gmail.com>
2024-12-09 11:28:02 -0300

    Deleted unused constants

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -18,23 +18,9 @@ export const BABEL_PARSER_OPTIONS = {
 export const REQUIRE_CALLEE_NAME = 'require';
 
 export const MESSAGES = {
-    WARNINGS: {
-        INVALID_METRIC: 'Invalid metric: ',
-        UNRECOGNIZED_METRIC: 'Metric is not recognized.',
-    },
     ERRORS: {
         ERROR_ON_FILE: 'Error on file',
-        ERROR_TRAVERSING_AST: 'Error while traversing the AST of file',
-        ERROR_TRAVERSING_AST_MESSAGE: 'Error while traversing AST:',
-        ERROR_FETCHING_ASTS: 'Error while fetching the ASTs:',
-        FAILED_FETCHING_ASTS: 'Failed to fetch the ASTs.',
         ERROR_READING_IGNORE_FILE: 'Error reading .ignore file at',
-        FAILED_TO_PARSE_FILE: 'Failed to parse file: ',
-        LOADING_METRIC: 'Error loading metric',
-        FROM: ' from ',
         ERROR_NO_METRICS: 'No metrics provided and useDefaultMetrics is set to false',
-    },
-    INFO: {
-        DONE: 'Done!',
     }
 };
