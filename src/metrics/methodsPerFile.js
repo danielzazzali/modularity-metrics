655141ca2f08a51848f2b3c905a62f5e55fa01a3
Daniel Zazzali <dazazzali@gmail.com>
2024-12-29 15:50:38 -0300

    Metric methods Per File v1.0

--- src/metrics/methodsPerFile.js
+++ src/metrics/methodsPerFile.js
@@ -1,58 +1,83 @@
 const state = {
     metricName: "Methods Per File",
-    description: "This metric counts the number of methods in each file.",
-    version: "0.0.1",
-    results: {},
-    currentFile: null
+    description: "This metric extracts the names of all methods in each file, excluding class methods",
+    version: "1.0",
+    result: {},
+    currentFile: null,
+    currentDir: null
 };
 
 const visitors = {
     Program(pathNode, state) {
-        state.currentFile = pathNode.node.loc.filename;
-    },
-    ClassMethod(pathNode, state) {
-        const fileName = state.currentFile;
-        if (!state.results[fileName]) {
-            state.results[fileName] = [];
+        const filePath = pathNode.parent.loc.filePath;
+        const fileName = pathNode.node.loc.filename;
+        const dirName = filePath.substring(0, filePath.lastIndexOf('/'));
+
+        state.currentFile = fileName;
+        state.currentDir = dirName;
+
+        if (!state.result[state.currentDir]) {
+            state.result[state.currentDir] = {};
         }
-        state.results[fileName].push(pathNode.node.key.name);
+
+        state.result[state.currentDir][state.currentFile] = {
+            methods: []
+        };
     },
     ObjectMethod(pathNode, state) {
-        const fileName = state.currentFile;
-        if (!state.results[fileName]) {
-            state.results[fileName] = [];
+        if (pathNode.node.key && pathNode.node.key.name) {
+            const methodName = pathNode.node.key.name;
+            state.result[state.currentDir][state.currentFile].methods.push(methodName);
         }
-        state.results[fileName].push(pathNode.node.key.name);
     },
     FunctionDeclaration(pathNode, state) {
-        const fileName = state.currentFile;
-        if (!state.results[fileName]) {
-            state.results[fileName] = [];
+        if (pathNode.node.id && pathNode.node.id.name) {
+            const methodName = pathNode.node.id.name;
+            state.result[state.currentDir][state.currentFile].methods.push(methodName);
         }
-        state.results[fileName].push(pathNode.node.id.name);
     },
     ArrowFunctionExpression(pathNode, state) {
-        const fileName = state.currentFile;
-        if (pathNode.parent.type === 'VariableDeclarator' && pathNode.parent.id.type === 'Identifier') {
-            if (!state.results[fileName]) {
-                state.results[fileName] = [];
-            }
-            state.results[fileName].push(pathNode.parent.id.name);
+        if (pathNode.parent.type === 'VariableDeclarator' && pathNode.parent.id && pathNode.parent.id.type === 'Identifier') {
+            state.result[state.currentDir][state.currentFile].methods.push(pathNode.parent.id.name);
         }
     },
     FunctionExpression(pathNode, state) {
-        const fileName = state.currentFile;
-        if (pathNode.parent.type === 'VariableDeclarator' && pathNode.parent.id.type === 'Identifier') {
-            if (!state.results[fileName]) {
-                state.results[fileName] = [];
+        if (pathNode.parent.type === 'VariableDeclarator' && pathNode.parent.id && pathNode.parent.id.type === 'Identifier') {
+            state.result[state.currentDir][state.currentFile].methods.push(pathNode.parent.id.name);
+        } else if (pathNode.parent.type === 'CallExpression' && pathNode.parent.callee.type === 'MemberExpression' && pathNode.parent.callee.property.name === 'bind') {
+            const methodName = pathNode.parent.callee.object.name;
+            state.result[state.currentDir][state.currentFile].methods.push(methodName);
+        } else if (pathNode.parent.type === 'CallExpression' && pathNode.parent.callee.type === 'Identifier' && pathNode.parent.callee.name === 'setTimeout') {
+            state.result[state.currentDir][state.currentFile].methods.push('anonymousFunction');
+        }
+    },
+    CallExpression(pathNode, state) {
+        if (pathNode.node.callee.type === 'FunctionExpression') {
+            state.result[state.currentDir][state.currentFile].methods.push('IIFE');
+        }
+    },
+    Property(pathNode, state) {
+        if (pathNode.node.value && (pathNode.node.value.type === 'FunctionExpression' || pathNode.node.value.type === 'ArrowFunctionExpression')) {
+            if (pathNode.node.computed && pathNode.node.key.type === 'Literal') {
+                const methodName = pathNode.node.key.value;
+                state.result[state.currentDir][state.currentFile].methods.push(methodName);
+            } else if (pathNode.node.key.type === 'Identifier') {
+                const methodName = pathNode.node.key.name;
+                state.result[state.currentDir][state.currentFile].methods.push(methodName);
             }
-            state.results[fileName].push(pathNode.parent.id.name);
+        }
+    },
+    VariableDeclarator(pathNode, state) {
+        if (pathNode.node.init && pathNode.node.init.type === 'CallExpression' && pathNode.node.init.callee.type === 'MemberExpression' && pathNode.node.init.callee.property.name === 'bind') {
+            const methodName = pathNode.node.id.name;
+            state.result[state.currentDir][state.currentFile].methods.push(methodName);
         }
     }
 };
 
 function postProcessing(state) {
     delete state.currentFile;
+    delete state.currentDir;
 }
 
 export { state, visitors, postProcessing };
