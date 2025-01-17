7f2e0fe21007bd7558fcf8c964bdae0d10adf3d2
Daniel Zazzali <dazazzali@gmail.com>
2025-01-16 21:33:01 -0300

    Improved classesPerFile metric

--- src/metrics/classesPerFile.js
+++ src/metrics/classesPerFile.js
@@ -1,48 +1,36 @@
 const state = {
     metricName: "Classes Per File",
-    description: "This metric extracts the names of all classes in each file",
-    version: "1.0",
+    description: "Extracts the names of all classes in each file",
+    version: "1.1",
     result: {},
-    currentFile: null,
-    currentDir: null
+    filePath: null
 };
 
 const visitors = {
     Program(pathNode, state) {
-        const filePath = pathNode.parent.loc.filePath;
-        const fileName = pathNode.node.loc.filename;
-        const dirName = filePath.substring(0, filePath.lastIndexOf('/'));
-
-        state.currentFile = fileName;
-        state.currentDir = dirName;
-
-        if (!state.result[state.currentDir]) {
-            state.result[state.currentDir] = {};
+        state.filePath = pathNode.parent.loc.filePath;
+        if (!state.result[state.filePath]) {
+            state.result[state.filePath] = [];
         }
-
-        state.result[state.currentDir][state.currentFile] = {
-            classes: []
-        };
     },
-    ClassDeclaration(path, state) {
-        const className = path.node.id.name;
-        state.result[state.currentDir][state.currentFile].classes.push(className);
+    ClassDeclaration(pathNode, state) {
+        const className = pathNode.node.id.name;
+        state.result[state.filePath].push(className);
     },
-    ClassExpression(path, state) {
-        const parent = path.parent;
+    ClassExpression(pathNode, state) {
+        const parentNode = pathNode.parent
         let className = 'AnonymousClass';
 
-        if (parent.type === 'VariableDeclarator' && parent.id.type === 'Identifier') {
-            className = parent.id.name;
+        if(parentNode.type === 'VariableDeclarator' && parentNode.id.type === 'Identifier') {
+            className = parentNode.id.name;
         }
 
-        state.result[state.currentDir][state.currentFile].classes.push(className);
+        state.result[state.filePath].push(className);
     }
 };
 
 const postProcessing = (state) => {
-    delete state.currentFile;
-    delete state.currentDir;
+    delete state.filePath;
 };
 
 export { state, visitors, postProcessing };
