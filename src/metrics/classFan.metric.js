f9fe797edcdd726dd4c949aa9297ed57dc03dc7c
Daniel Zazzali <dazazzali@gmail.com>
2025-06-02 22:04:54 -0400

    update

--- src/metrics/classFan.metric.js
+++ src/metrics/classFan.metric.js
@@ -1,3 +1,6 @@
+import traverse from "@babel/traverse";
+import * as t from "@babel/types";
+
 const state = {
     name: "Class Fan-In and Fan-Out",
     description: "",
@@ -12,28 +15,55 @@ const visitors = {
         state.currentFile = path.node.filePath;
         state.result[state.currentFile] = state.dependencies['classes-per-file'][state.currentFile];
     },
-
-    ClassMethod(path) {
-
-    },
-
-    ClassPrivateMethod(path) {
-
-    },
-
-    ClassProperty(path) {
-
-    },
-
-    ClassPrivateProperty(path) {
-
-    },
 };
 
 
+function calculateFan(result) {
+    for (const file in result) {
+        for (const className in result[file]) {
+            countFanOut(className);
+        }
+    }
+}
+
+
+function countFanOut(classSearched) {
+    for (const file in state.result) {
+        for (const className in state.result[file]) {
+
+            const node = state.result[file][className];
+            state.result[file][className]['fan-out'] = {};
+
+            const reconstructedAST = t.file(t.program([
+                node
+            ]));
+
+            traverse.default(reconstructedAST, {
+                NewExpression(innerPath) {
+                    if (innerPath.node.callee.type === 'Identifier' && innerPath.node.callee.name === classSearched) {
+                        if (!state.result[file][className]['fan-out'][classSearched]) {
+                            state.result[file][className]['fan-out'][classSearched] = {};
+                        }
+
+                        if (!(state.result[file][className]['fan-out'][classSearched]['newExpression'])) {
+                            state.result[file][className]['fan-out'][classSearched]['newExpression'] = 0;
+                        }
+
+                        state.result[file][className]['fan-out'][classSearched]['newExpression']++;
+                    }
+                },
+            });
+        }
+    }
+}
+
+
 function postProcessing(state){
+    calculateFan(state.result);
     if (state.currentFile) delete state.currentFile;
     if (state.dependencies) delete state.dependencies;
+
+    console.log(JSON.stringify(state, null, 2));
 }
 
 export { state, visitors, postProcessing };
